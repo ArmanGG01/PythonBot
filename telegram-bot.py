cc6ff44ea7a18e13ab6a4f92710e77962258791e
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2023-08-13 13:49:44 +0700

    Update telegram-bot.py

--- telegram-bot.py
+++ telegram-bot.py
@@ -1,50 +1,34 @@
-define('BOT_TOKEN', '6315172994:AAE-nSTwK6xgewRkiybt2YGPvZ8kBZDzbrw');
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+import random
 
-function sendMessage($chatID, $message, $replyMarkup = null) {
-    $url = 'https://api.telegram.org/bot' . BOT_TOKEN . '/sendMessage';
-    $data = array('chat_id' => $chatID, 'text' => $message, 'reply_markup' => $replyMarkup);
-    $options = array(
-        'http' => array(
-            'method'  => 'POST',
-            'content' => http_build_query($data),
-            'header'  => "Content-Type: application/x-www-form-urlencoded\r\n"
-        )
-    );
-    $context  = stream_context_create($options);
-    file_get_contents($url, false, $context);
-}
+# Fungsi untuk menangani perintah /start
+def start(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text("Halo! Saya adalah bot gabut. Kirim pesan apa pun untuk berbincang.")
 
-$update = json_decode(file_get_contents("php://input"), TRUE);
-$chatID = $update['message']['chat']['id'];
-$message = $update['message']['text'];
+# Fungsi untuk menangani pesan masuk
+def echo(update: Update, context: CallbackContext) -> None:
+    responses = ["Haha iya", "Wah seru ya!", "Keren!", "Ngomong-ngomong, gimana kabarmu?"]
+    response = random.choice(responses)
+    update.message.reply_text(response)
 
-$responses = array(
-    "Halo! Bagaimana kabarmu?",
-    "Apa kabar?",
-    "Sedang apa?",
-    "Punya rencana apa hari ini?",
-    "Hobi kamu apa?",
-    "Aku suka berbicara tentang cuaca.",
-    "Ada yang menarik yang terjadi baru-baru ini?"
-);
+def main() -> None:
+    # Ganti TOKEN_BOT_ANDA dengan token bot yang Anda dapatkan dari BotFather di Telegram
+    updater = Updater("6315172994:AAE-nSTwK6xgewRkiybt2YGPvZ8kBZDzbrw")
 
-$inlineKeyboard = json_encode(array(
-    'inline_keyboard' => array(
-        array(
-            array('text' => 'Halo', 'callback_data' => 'halo'),
-            array('text' => 'Apa kabar?', 'callback_data' => 'apa_kabar')
-        )
-    )
-));
+    dispatcher = updater.dispatcher
 
-if (strtolower($message) == "/start") {
-    $response = "Halo! Aku adalah bot gabut. Apa yang ingin kamu tanyakan?";
-} elseif (strtolower($message) == "/options") {
-    sendMessage($chatID, "Pilih opsi:", $inlineKeyboard);
-    exit;
-} else {
-    $response = $responses[array_rand($responses)];
-}
+    # Menangani perintah /start
+    dispatcher.add_handler(CommandHandler("start", start))
 
-sendMessage($chatID, $response);
-?>
+    # Menangani pesan masuk
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+
+    # Memulai polling untuk mendapatkan pesan masuk
+    updater.start_polling()
+
+    # Menjaga bot berjalan hingga Anda berhenti secara manual
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
