442d7c310f8ec2b7e8cb891926bf4d040ead996a
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2023-08-13 13:22:55 +0700

    Create telegram-bot.py

---
+++ telegram-bot.py
@@ -0,0 +1,31 @@
+import requests
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext, InlineQueryHandler
+from telegram.inline.inputtextmessagecontent import InputTextMessageContent
+from telegram.inline.queryresult import InlineQueryResultArticle
+
+TOKEN = "6315172994:AAE-nSTwK6xgewRkiybt2YGPvZ8kBZDzbrw"
+
+def start(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text("Halo! Saya bot gabut.")
+
+def options(update: Update, context: CallbackContext) -> None:
+    keyboard = [
+        [InlineQueryResultArticle(id="1", title="Halo Nyet", input_message_content=InputTextMessageContent("Anda memilih opsi 1."))],
+        [InlineQueryResultArticle(id="2", title="Opsi 2", input_message_content=InputTextMessageContent("Anda memilih opsi 2."))],
+        [InlineQueryResultArticle(id="3", title="Opsi 3", input_message_content=InputTextMessageContent("Anda memilih opsi 3."))]
+    ]
+    update.inline_query.answer(keyboard)
+
+def main():
+    updater = Updater(token=TOKEN, use_context=True)
+    dispatcher = updater.dispatcher
+    
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("options", options))
+    
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == "__main__":
+    main()
